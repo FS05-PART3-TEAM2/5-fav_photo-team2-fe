85c19cbef5987b606e854468fc8cd2cb4b6cc5d4
sejeong <ygn06024@naver.com>
2025-03-26 16:22:11 +0900

    feat: buttonType Primary/Secondary 추가

--- src/stories/BtnTest.stories.tsx
+++ src/stories/BtnTest.stories.tsx
@@ -13,18 +13,23 @@ export default meta;
 // StoryFn을 사용하여 스토리 정의
 export const BtnTest: StoryFn = () => {
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-500">
       <div className="p-[20px] w-[345px] md:w-[342px] lg:w-[440px]">
         <ThickPrimaryBtn>포토카드 구매하기</ThickPrimaryBtn>
+        <ThickPrimaryBtn buttonType="Secondary">Secondary 버튼</ThickPrimaryBtn>
       </div>
       <div className="p-[20px] w-[345px] md:w-[440px] lg:w-[520px]">
         <ThinPrimaryBtn>포토카드 교환하기</ThinPrimaryBtn>
+        <ThinPrimaryBtn buttonType="Secondary">
+          포토카드 교환하기
+        </ThinPrimaryBtn>
       </div>
       <div className="p-[20px] w-[345px] md:w-[440px] lg:w-[520px]">
         <ThinPrimaryBtn disabled>포토카드 교환하기</ThinPrimaryBtn>
       </div>
       <div className="p-[20px] w-[72.5px] md:w-[141px] lg:w-[170px]">
         <XSPrimaryBtn>승인</XSPrimaryBtn>
+        <XSPrimaryBtn buttonType="Secondary">승인</XSPrimaryBtn>
       </div>
     </div>
   );
