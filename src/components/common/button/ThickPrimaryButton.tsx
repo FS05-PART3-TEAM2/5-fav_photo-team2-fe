e4143eb435d3ccfb796cd37a29e9558adf0d11d9
sejeong <ygn06024@naver.com>
2025-03-26 10:16:12 +0900

    refactor: PrimaryButton width prop받는 방식 추가 + ThinButton 스타일 수정

--- src/components/common/button/ThickPrimaryButton.tsx
+++ src/components/common/button/ThickPrimaryButton.tsx
@@ -2,19 +2,20 @@ import { ButtonHTMLAttributes, forwardRef } from "react";
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
   isLoading?: boolean; // 로딩 상태 추가
+  width?: string; // 너비를 props로 받음
 };
 
 const ThickPrimaryButton = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ isLoading, children, className, disabled, ...props }, ref) => {
+  ({ isLoading, children, className, disabled, width, ...props }, ref) => {
     // 기본 스타일
     const baseStyles =
       "flex items-center justify-center rounded-[2px] px-[144px] py-[25px] gap-[10px] text-dark font-bold leading-[100%] whitespace-nowrap";
 
     // 화면 크기에 따라 스타일 변경 (반응형)
     const responsiveStyles = `
-      w-[345px] h-[75px] px-[25px] gap-[10px] text-[18px]
-      md:w-[342px] md:h-[75px] text-[18px]
-      lg:w-[440px] lg:h-[80px] text-[20px]
+      h-[75px] px-[25px] gap-[10px] text-[18px]
+      md: md:h-[75px] text-[18px]
+      lg: lg:h-[80px] text-[20px]
     `;
 
     // 활성화된 버튼 스타일 (main 색상 적용)
@@ -32,6 +33,8 @@ const ThickPrimaryButton = forwardRef<HTMLButtonElement, ButtonProps>(
           ${isLoading || disabled ? disabledStyles : enabledStyles} 
           ${className}
         `}
+        style={{ width }} // width를 props로 받아 적용
+        //<ThickPrimaryButton width="345px"> 이런식으로 써주세요!
         disabled={isLoading || disabled} // 로딩 중일 때도 비활성화
         {...props}
       >
