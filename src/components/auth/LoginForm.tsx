abd64ee748454530c096e6101789af3fea3ef437
민지영 <kuck523@gmail.com>
2025-03-27 10:34:39 +0900

    🧪 Test Form
    test form

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,1 +1,37 @@
- 
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { loginSchema, LoginFormSchema } from '@/schema/formSchema';
+import InputText from '@/components/common/input/Input';
+import Password from '@/components/common/input/Password';
+import Textarea from '../common/input/Textarea';
+import Search from '../common/input/Search';
+import { useState } from 'react';
+
+export default function LoginForm() {
+  const {
+    control,
+    formState: { isValid, isSubmitting },
+  } = useForm<LoginFormSchema>({
+    resolver: zodResolver(loginSchema),
+    mode: 'onChange',
+    defaultValues: {
+      email: '',
+      password: '',
+      wishContent: '',
+    },
+  });
+  const [keyword, setKeyword] = useState('');
+
+  return (
+    <form className="max-w-110 md:w-110 xl:w-130 flex flex-col gap-[30px]">
+      <InputText name="email" control={control} />
+      <Password name="password" control={control} />
+      <Textarea name="wishContent" control={control} />
+      <Search onSearch={setKeyword} />
+      keyword: {keyword}
+      <button disabled={!isValid || isSubmitting}>로그인</button>
+    </form>
+  );
+}
