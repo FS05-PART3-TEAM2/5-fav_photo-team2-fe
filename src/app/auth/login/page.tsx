ab1eb22bf9a00f91f4624accc1a907ef8f3c993a
민지영 <kuck523@gmail.com>
2025-03-27 11:54:55 +0900

    🧪 Test page
    test page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,1 +1,17 @@
- 
+'use client';
+
+import LoginForm from '@/components/auth/LoginForm';
+import Search from '@/components/input/Search';
+import { useState } from 'react';
+
+export default function Page() {
+  const [keyword, setKeyword] = useState('');
+
+  return (
+    <>
+      <Search onSearch={setKeyword} />
+      <LoginForm />
+      <p>검색어: {keyword}</p>
+    </>
+  );
+}
