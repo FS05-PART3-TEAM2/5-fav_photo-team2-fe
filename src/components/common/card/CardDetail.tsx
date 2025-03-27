2b870f752b7feadcdec5d8662532cbd069bc9215
seha <hansaemj20@gmail.com>
2025-03-27 18:58:15 +0900

    design: 설명문 2줄까지로 제한

--- src/components/common/card/CardDetail.tsx
+++ src/components/common/card/CardDetail.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 type AmountText = "잔여" | "수량" | "보유량";
 
 interface CardDetailProps {
@@ -13,42 +15,41 @@ const CardDetail = ({ ...props }: CardDetailProps) => {
   const commonStyle =
     props.cardType === "details"
       ? "py-[30px] border-t border-gray-400"
-      : "py-[10px] md:py-[20px] border-t border-gray-400";
+      : "pt-[10px] md:pt-[20px] mt-[10px] md:mt-[20px] border-t border-gray-400";
   const descriptionStyle =
-    props.cardType === "details"
-      ? "font-normal text-[16px] lg:text-[18px]"
-      : "card-header__list";
+    props.cardType === "details" ? "font-normal text-[16px] lg:text-[18px]" : "card-header__list";
 
   return (
-    <div>
+    <div className="w-40">
       {props.description && (
-        <div className={`${descriptionStyle} ${commonStyle} text-white`}>
+        <div
+          className={clsx(`${descriptionStyle} ${commonStyle} text-white`, {
+            ["line-clamp-2 break-all"]: props.cardType === "list",
+          })}
+        >
           {props.description}
         </div>
       )}
-      {props.price &&
-        props.availableAmount &&
-        props.totalAmount &&
-        props.amountText === "잔여" && (
-          <div className={`${commonStyle} flex flex-col gap-[10px]`}>
-            <div className="flex justify-between items-center">
-              <div className="text-gray-300 text-[18px] font-normal">가격</div>
-              <div className="text-whit  text-[20px] font-bold lg:text-[24px]">
-                {props.price}&nbsp;P
-              </div>
-            </div>
-            <div className="flex justify-between items-center">
-              <div className="text-gray-300 text-[18px] font-normal">잔여</div>
-              <div className="flex items-center text-[20px] lg:text-[24px]">
-                <p className="text-white font-bold">{props.availableAmount}</p>
-                <p className="text-gray-300 font-normal">
-                  &nbsp;{"/"}
-                  &nbsp;{props.totalAmount}
-                </p>
-              </div>
+      {props.price && props.availableAmount && props.totalAmount && props.amountText === "잔여" && (
+        <div className={`${commonStyle} flex flex-col gap-[10px]`}>
+          <div className="flex justify-between items-center">
+            <div className="text-gray-300 text-[18px] font-normal">가격</div>
+            <div className="text-whit  text-[20px] font-bold lg:text-[24px]">
+              {props.price}&nbsp;P
             </div>
           </div>
-        )}
+          <div className="flex justify-between items-center">
+            <div className="text-gray-300 text-[18px] font-normal">잔여</div>
+            <div className="flex items-center text-[20px] lg:text-[24px]">
+              <p className="text-white font-bold">{props.availableAmount}</p>
+              <p className="text-gray-300 font-normal">
+                &nbsp;{"/"}
+                &nbsp;{props.totalAmount}
+              </p>
+            </div>
+          </div>
+        </div>
+      )}
       {props.price &&
         props.availableAmount &&
         props.totalAmount &&
@@ -61,9 +62,7 @@ const CardDetail = ({ ...props }: CardDetailProps) => {
               </div>
             </div>
             <div className="flex justify-between items-center">
-              <div className="text-gray-300 text-[18px] font-normal">
-                {props.amountText}
-              </div>
+              <div className="text-gray-300 text-[18px] font-normal">{props.amountText}</div>
               <div className="flex items-center">
                 <p className="text-white text-[20px] lg:text-[24px] font-bold">
                   {props.availableAmount}
@@ -81,13 +80,9 @@ const CardDetail = ({ ...props }: CardDetailProps) => {
             </div>
           </div>
           <div className="flex justify-between items-center">
-            <div className="text-gray-300 text-[18px] font-normal">
-              {props.amountText}
-            </div>
+            <div className="text-gray-300 text-[18px] font-normal">{props.amountText}</div>
             <div className="flex items-center">
-              <p className="text-white text-[20px] lg:text-[24px] font-bold">
-                {props.totalAmount}
-              </p>
+              <p className="text-white text-[20px] lg:text-[24px] font-bold">{props.totalAmount}</p>
             </div>
           </div>
         </div>
