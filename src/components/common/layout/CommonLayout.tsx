7c0d5c378383f1e15a7a82434b1344e9fa0d3ae0
HoeunRyu <hoeun9565@gmail.com>
2025-03-27 14:00:34 +0900

    fix: CommonLayout 배경 설정 + 데스크탑 여백 수정

--- src/components/common/layout/CommonLayout.tsx
+++ src/components/common/layout/CommonLayout.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 // 로그인/회원가입 페이지 제외 모든 필요한 페이지에서 사용
 export const CommonLayout = ({ children }: { children: ReactNode }) => {
   const styles = {
-    wrapper: 'flex flex-col min-h-screen',
-    main: 'flex flex-1 flex-col items-center w-[100%]',
-    content: 'max-w-[1480px] w-[100%] overflow-x-auto mx-auto px-[15px] md:px-[20px] lg:px-0',
+    wrapper: "flex flex-col min-h-screen bg-dark",
+    main: "flex flex-1 flex-col items-center w-[100%]",
+    content: "max-w-[1520px] w-[100%] overflow-x-auto mx-auto px-[15px] md:px-[20px]",
   };
 
   return (
