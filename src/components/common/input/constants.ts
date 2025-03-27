fccb50bce7d90ff6288d5d41d9138a1e5736edfb
민지영 <kuck523@gmail.com>
2025-03-27 13:29:23 +0900

    📝 수정: 싱글쿼터->더블쿼터, 테스트 폼 페이지 삭제
    테스트 폼, 페이지 코드는 PR에 정리

--- src/components/common/input/constants.ts
+++ src/components/common/input/constants.ts
@@ -1,43 +1,43 @@
 export const FORM_CONFIG = {
   Input: {
     photoCardName: {
-      label: '포토카드 이름',
-      placeholder: '포토카드 이름을 입력해 주세요',
+      label: "포토카드 이름",
+      placeholder: "포토카드 이름을 입력해 주세요",
     },
-    email: { label: '이메일', placeholder: '이메일을 입력해주세요' },
-    nick: { label: '닉네임', placeholder: '닉네임을 입력해주세요' },
-    price: { label: '가격', placeholder: '가격을 입력해 주세요' },
-    stock: { label: '총 발행량', placeholder: '총 발행량을 입력해 주세요' },
+    email: { label: "이메일", placeholder: "이메일을 입력해주세요" },
+    nick: { label: "닉네임", placeholder: "닉네임을 입력해주세요" },
+    price: { label: "가격", placeholder: "가격을 입력해 주세요" },
+    stock: { label: "총 발행량", placeholder: "총 발행량을 입력해 주세요" },
   },
   Textarea: {
     photoCardContent: {
-      label: '포토카드 설명',
-      placeholder: '카드 설명을 입력해 주세요',
+      label: "포토카드 설명",
+      placeholder: "카드 설명을 입력해 주세요",
       height: 220, // textarea height
     },
     wishContent: {
-      label: '교환 희망 설명',
-      placeholder: '교환 희망 설명을 입력해 주세요',
+      label: "교환 희망 설명",
+      placeholder: "교환 희망 설명을 입력해 주세요",
       height: 120, // textarea height
     },
   },
   Password: {
-    password: { label: '비밀번호', placeholder: '비밀번호를 입력해주세요' },
+    password: { label: "비밀번호", placeholder: "비밀번호를 입력해주세요" },
     passwordConfirm: {
-      label: '비밀번호 확인',
-      placeholder: '비밀번호를 확인해주세요',
+      label: "비밀번호 확인",
+      placeholder: "비밀번호를 확인해주세요",
     },
   },
   Dropdown: {
     grade: {
-      label: '등급',
-      placeholder: '등급을 선택해 주세요',
-      options: ['COMMON', 'RARE', 'SUPER RARE', 'LEGENDARY'],
+      label: "등급",
+      placeholder: "등급을 선택해 주세요",
+      options: ["COMMON", "RARE", "SUPER RARE", "LEGENDARY"],
     },
     genre: {
-      label: '장르',
-      placeholder: '장르를 선택해 주세요',
-      options: ['여행', '풍경', '인물', '사물'],
+      label: "장르",
+      placeholder: "장르를 선택해 주세요",
+      options: ["여행", "풍경", "인물", "사물"],
     },
   },
 };
