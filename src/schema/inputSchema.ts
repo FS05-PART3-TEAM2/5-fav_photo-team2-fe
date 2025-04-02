64313a8447e36281246cc3d1472e573551115d87
민지영 <kuck523@gmail.com>
2025-04-02 19:08:35 +0900

    ♻️ Update password schema
    Update password schema

--- src/schema/inputSchema.ts
+++ src/schema/inputSchema.ts
@@ -7,6 +7,24 @@ const contentSchema = z
   .min(10, "10자 이상 입력해주세요")
   .max(100, "100자 이내로 입력해주세요"); // 설명은 10자 이상 100자 이내로 입력받음
 
+const validatePassword = (password: string, ctx: z.RefinementCtx) => {
+  // 비밀번호 유효성 검사
+  const errors = [];
+
+  if (password.length < 9) errors.push("9자 이상");
+  if (!/[A-Z]/.test(password)) errors.push("대문자 1개 이상");
+  if (!/[a-z]/.test(password)) errors.push("소문자 1개 이상");
+  if (!/[0-9]/.test(password)) errors.push("숫자 1개 이상");
+  if (!/[^A-Za-z0-9]/.test(password)) errors.push("특수기호 1개 이상");
+
+  if (errors.length > 0) {
+    ctx.addIssue({
+      code: z.ZodIssueCode.custom,
+      message: `비밀번호는 ${errors.join(", ")}을 포함해야 합니다.`,
+    });
+  }
+};
+
 // 입력 폼 스키마
 export const InputSchema = z.object({
   email: z.string().email("잘못된 이메일입니다"), // 이메일
@@ -14,13 +32,7 @@ export const InputSchema = z.object({
     .string()
     .min(1, "닉네임을 입력해주세요") // 빈 값 방지
     .max(6, "닉네임은 6자 이내로 입력해주세요"), // 닉네임
-  password: z
-    .string()
-    .min(9, "비밀번호를 9자 이상 입력해주세요") // 비밀번호 최소 길이
-    .regex(/[A-Z]/, "비밀번호에 대문자를 포함해주세요") // 대문자 포함
-    .regex(/[a-z]/, "비밀번호에 소문자를 포함해주세요") // 소문자 포함
-    .regex(/[0-9]/, "비밀번호에 숫자를 포함해주세요") // 숫자 포함
-    .regex(/[^A-Za-z0-9]/, "비밀번호에 특수기호를 포함해주세요"), // 특수기호 포함
+  password: z.string().superRefine(validatePassword), // 비밀번호
   passwordConfirm: z.string(), // 비밀번호 확인
 
   photoCardName: z.string().max(30, "최대 30자 이내로 입력해주세요"), // 포토카드 이름
