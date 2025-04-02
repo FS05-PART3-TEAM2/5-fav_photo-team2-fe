e60daf427502b4e2d61103bab600d2ea91f99a74
HoeunRyu <hoeun9565@gmail.com>
2025-04-02 13:25:41 +0900

    마켓플레이스 상세 구매자 뷰 작업중 - 공통컴포넌트 카드요소 디테일 수정

---
+++ src/components/market/detail/consumer/MyExchangeOffers.tsx
@@ -0,0 +1,29 @@
+import { ExchangeCardDto } from "@/types/photocard.types";
+import { SectionTitle } from "../SectionTitle";
+import { MyExchangeCard } from "./MyExchangeCard";
+
+interface MyExchangeOffersProps {
+  data: ExchangeCardDto[] | null;
+}
+
+export const MyExchangeOffers: React.FC<MyExchangeOffersProps> = ({ data }) => {
+  const isEmpty = data === null || data.length === 0;
+
+  return (
+    <div className="w-[100%] flex flex-col gap-[40px] lg:gap-[60px]">
+      <SectionTitle title="내가 제시한 교환 목록" />
+      {isEmpty ? (
+        <div className="w-[100%] h-[200px] flex items-center justify-center">
+          <p className="text-gray-100 text-[18px] md:text-[24px] font-normal">
+            교환 제시 내역이 없습니다.
+          </p>
+        </div>
+      ) : (
+        // 교환 제시 카드 목록
+        <div className="w-[100%] grid grid-cols-2 lg:grid-cols-3 gap-[5px] md:gap-[20px] lg:gap-[40px]">
+          {data?.map(item => <MyExchangeCard key={item.id} data={item} />)}
+        </div>
+      )}
+    </div>
+  );
+};
