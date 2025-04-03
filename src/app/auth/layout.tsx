26d00639ede63035841e27b59cee21a4ebdbbd5e
민지영 <kuck523@gmail.com>
2025-04-03 09:25:05 +0900

    ♻️ Change logo from image to font
    Change logo from image to font

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,18 +1,12 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <section className="px-4 py-20 flex flex-col items-center justify-center text-center">
       <Link href="/">
-        <Image
-          src="/assets/icons/logo.png"
-          alt="최애의 포토"
-          width={330}
-          height={60}
-          priority
-          className="w-[190px] h-[35px] md:w-[330px] md:h-[60px] mb-20"
-        />
+        <h1 className="font-BR-B text-5xl md:text-7xl mb-20">
+          최애<span className="text-main">의</span>포토
+        </h1>
       </Link>
       {children}
     </section>
