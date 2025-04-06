3a20edbfa386671a76ca043df25bbe012b5e7753
sejeong <ygn06024@naver.com>
2025-04-07 03:05:24 +0900

    refactor: 타입변경에 따른 에러부분 수정 및 리뷰 반영

--- src/components/market/detail/SectionTitle.tsx
+++ src/components/market/detail/SectionTitle.tsx
@@ -8,7 +8,6 @@ export const SectionTitle: React.FC<SectionTitleProps> = ({ title, button }) => 
     <div className="w-[100%] flex items-center justify-between pb-[10px] md:pb-[20px] border-b-[2px] border-gray-100">
       <p className="market-detail-title">{title}</p>
       <div className="hidden md:block">{button}</div>
-      {}
     </div>
   );
 };
