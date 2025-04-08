f560aceddfcca079a78384642e1c788d6e7b9544
seha <hansaemj20@gmail.com>
2025-04-08 19:37:08 +0900

    fix: 로그아웃 이후 로그인 페이지로 이동

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,8 +7,12 @@ export function middleware(request: NextRequest) {
   // ✅ "/market"은 허용
   // 🔒 "/market/..." 는 로그인 필요
   const isProtectedMarketPath = pathname.startsWith("/market/") && pathname !== "/market";
+  const isProtectedMySalePath = pathname.startsWith("/my-sales");
+  const isProtectedMyPhotoPath = pathname.startsWith("/my-photos");
 
-  if (isProtectedMarketPath && !accessToken) {
+  const isProtected = isProtectedMarketPath || isProtectedMySalePath || isProtectedMyPhotoPath;
+
+  if (isProtected && !accessToken) {
     const loginUrl = new URL("/auth/login", request.url);
     return NextResponse.redirect(loginUrl);
   }
@@ -17,5 +21,9 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/market/:path*",
+  matcher: [
+    "/market/:path*", // /market/123 등
+    "/my-sales/:path*", // /my-sale/abc 등
+    "/my-photos/:path*", // /my-photo/xyz 등
+  ],
 };
