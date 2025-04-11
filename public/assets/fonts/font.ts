4c23e2ae343446c84536a5762d3500b47b11fd3c
Ripponcheese <hayooncode@gmail.com>
2025-04-11 22:12:37 +0900

    fix: 빌드오류 해결

--- public/assets/fonts/font.ts
+++ public/assets/fonts/font.ts
@@ -1,7 +1,9 @@
 import localFont from "next/font/local";
 
 export const baskinBold = localFont({
-  src: "../fonts/BR-B.woff2", // 폰트 파일 경로 (올바른 경로 확인 필요)
-  display: "swap", // 폰트 로딩 전략
-  variable: "--font-BR-B", // CSS 변수로 폰트 사용
+  src: "./BR-B.woff2",
+  display: "swap",
+  variable: "--font-BR-B",
+  preload: true,
+  fallback: ["system-ui", "arial"],
 });
