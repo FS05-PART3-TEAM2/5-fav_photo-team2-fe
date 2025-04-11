76673aeb62064195f98204b34d4aa2ed0d7b16b6
민지영 <kuck523@gmail.com>
2025-04-11 15:40:48 +0900

    ♻️ Update destination
    Update destination

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*", // 클라이언트가 요청하는 주소
-        destination: `https://five-fav-photo-team2-be.onrender.com/api/:path*`, // 실제 백엔드 서버 주소
-        // destination: `http://localhost:8080/api/:path*`, // 실제 백엔드 서버 주소
+        destination: `https://five-fav-photo-team2-be-1zgs.onrender.com/api/:path*`, // 실제 백엔드 서버 주소
       },
     ];
   },
