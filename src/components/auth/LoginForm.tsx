5e599e5c031d2b4dd506ff659f731d68bfe1c4d1
민지영 <kuck523@gmail.com>
2025-04-14 15:37:00 +0900

    ♻️ Feat: 로그인 성공 후 사용자 마켓 페이지로 자동 이동 구현
    feat: 로그인 성공 후 사용자 마켓 페이지로 자동 이동 구현

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -9,8 +9,8 @@ import ThinBtn from "../common/button/ThinBtn";
 import { useActionState, useEffect } from "react";
 import loginAction from "@/lib/actions/login.action";
 import { useRouter } from "next/navigation";
-import useUserStore from "@/store/useUserStore";
 import { useSnackbarStore } from "@/store/useSnackbarStore";
+import useUserStore from "@/store/useUserStore";
 
 export default function LoginForm() {
   const {
@@ -26,25 +26,25 @@ export default function LoginForm() {
   });
   const [state, formAction, isPending] = useActionState(loginAction, null);
   const router = useRouter();
-  const { setUser } = useUserStore(); // 유저정보 상태 업데이트 함수 가져오기
+  const { setUser, isAuthenticated } = useUserStore(); // 유저정보 상태 업데이트 함수 가져오기
   const { openSnackbar } = useSnackbarStore(); // Snackbar 상태 업데이트 함수 가져오기
 
   useEffect(() => {
     if (!state) return;
 
-    console.log("Login state:", state); // 디버깅을 위한 로그 추가
-
     if (state.status) {
-      // 로그인 성공시
-      setUser(state.user); // zustand store에 사용자 정보 저장
+      setUser(state.user);
       openSnackbar("SUCCESS", "로그인 완료되었습니다.");
-      router.push("/"); // 로그인 성공 후 홈으로 이동,
-      // useRouter()가 클라이언트 내에서 상태를 유지한 채로 이동
-      // 완전히 새로고침 없이 페이지 이동이 되기 때문에 상태가 유지
     } else {
       openSnackbar("ERROR", state.message || "로그인에 실패했습니다.");
     }
-  }, [state, setUser, router]);
+  }, [state, setUser, openSnackbar]);
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.push("/market");
+    }
+  }, [isAuthenticated, router]);
 
   return (
     <form action={formAction} className="w-form">
